54960e51a863f75d27da8a3073bc4d28b04521a3
HongWei <limhongwei0622@gmail.com>
2023-03-26 20:24:18 +0800

    Added loading animation

--- gui/main.py
+++ gui/main.py
@@ -1,12 +1,32 @@
 import os
 import sys
+import time
+from ui import Loading
 from core import PageWindow, Home, Register, Record, Upload
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.uic import loadUi
+
+class SplashScreen(QtWidgets.QSplashScreen):
+    def __init__(self):
+        super(QtWidgets.QSplashScreen, self).__init__()
+        self.ui = Loading.Ui_Form()
+        self.ui.setupUi(self)
+        # self.setFixedWidth(1147)
+        # self.setFixedHeight(778)
+        # pixmap = QtGui.QPixmap("Image\\gradient-green.jpg")
+        # pixmap = pixmap.scaled(1147, 778, QtCore.Qt.KeepAspectRatio)
+        # self.setPixmap(pixmap)
+
+    def progress(self):
+        for i in range(101):
+            self.ui.progressBar.setValue(i)
+            QtWidgets.QApplication.processEvents()
+            time.sleep(0.1)
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent = None):
         super().__init__(parent)
-        
+
         self.stacked_widget = QtWidgets.QStackedWidget()
         self.setCentralWidget(self.stacked_widget)
         
@@ -35,6 +55,14 @@ class MainWindow(QtWidgets.QMainWindow):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv) 
+    # myapp = MainWindow()
+    # myapp.show()
+    splash = SplashScreen()
+    splash.show()
+    splash.progress()
+
     myapp = MainWindow()
-    myapp.show() 
+    myapp.show()
+
+    splash.finish(myapp)
     sys.exit(app.exec_())
