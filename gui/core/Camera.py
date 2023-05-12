fd1ae46fa2cb685711479f78b7b7dc5549569c3f
HongWei <limhongwei0622@gmail.com>
2023-05-12 20:51:37 +0800

    Added controller into Camera ui

--- gui/core/Camera.py
+++ gui/core/Camera.py
@@ -1,6 +1,8 @@
 from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.Qt import Qt
 from ui import Camera
 from core.PageWindow import PageWindow
+from core.RobotCar import RobotCar
 
 import os
 import sys
@@ -68,6 +70,23 @@ class WindowCamera(PageWindow):
 
         self.isCapturing = False
         self.isDetecting = False
+
+        self.car = RobotCar.RobotCar()
+        self.status = False
+
+        self.ui.horn_button.clicked.connect(self.off)
+
+        self.ui.up_button.pressed.connect(self.moveForward)
+        self.ui.up_button.released.connect(self.off)
+
+        self.ui.down_button.pressed.connect(self.moveBackward)
+        self.ui.down_button.released.connect(self.off)
+
+        self.ui.left_button.pressed.connect(self.moveLeft)
+        self.ui.left_button.released.connect(self.off)
+
+        self.ui.right_button.pressed.connect(self.moveRight)
+        self.ui.right_button.released.connect(self.off)
 
         self.start()
 
@@ -178,3 +197,47 @@ class WindowCamera(PageWindow):
         elif i == "disease":
             self.ui.pest_detection_button.setChecked(False)
             self.current_model = "disease"
+
+    def off(self):
+        if self.status:
+            print("Car Stop")
+            self.status = False
+            self.car.stop()
+
+    def moveForward(self):
+        self.status = True
+        print("Moving Forward")
+        self.car.forward()
+
+    def moveBackward(self):
+        self.status = True
+        print("Moving Backward")
+        self.car.backward()
+
+    def moveLeft(self):
+        self.status = True
+        print("Moving Left")
+        self.car.left()
+
+    def moveRight(self):
+        self.status = True
+        print("Moving Right")
+        self.car.right()
+
+    def keyPressEvent(self, event):
+        if (event.key() == Qt.Key_W or event.key() == Qt.Key_Up) and not event.isAutoRepeat():
+            self.moveForward()
+        elif (event.key() == Qt.Key_S or event.key() == Qt.Key_Down) and not event.isAutoRepeat():
+            self.moveBackward()
+        elif (event.key() == Qt.Key_A or event.key() == Qt.Key_Left) and not event.isAutoRepeat():
+            self.moveLeft()
+        elif (event.key() == Qt.Key_D or event.key() == Qt.Key_Right) and not event.isAutoRepeat():
+            self.moveRight()
+
+    def keyReleaseEvent(self, event):
+        forward_key = event.key() == Qt.Key_W or event.key() == Qt.Key_Up
+        backward_key = event.key() == Qt.Key_S or event.key() == Qt.Key_Down
+        left_key = event.key() == Qt.Key_A or event.key() == Qt.Key_Left
+        right_key = event.key() == Qt.Key_D or event.key() == Qt.Key_Right
+        if not event.isAutoRepeat() and (forward_key or backward_key or left_key or right_key):
+            self.off()
